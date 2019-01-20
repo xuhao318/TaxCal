871913b83d480d0dc2d5b5cd8b10ae2a2a37d3be
Hao Xu <xuhao318@gmail.com>
2019-01-19 23:44:14 -0500

    improved page

--- pages/tax/tax.js
+++ pages/tax/tax.js
@@ -62,5 +62,14 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  /**
+   * 
+   */
+  formSubmit(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset() {
+    console.log('form发生了reset事件')
   }
 })
