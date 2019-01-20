e2e2a4b5493b42214dc308cbe31ffca2aa467945
Hao Xu <xuhao318@gmail.com>
2019-01-19 23:30:06 -0500

    Add simple tax page

---
+++ pages/tax/tax.js
@@ -0,0 +1,66 @@
+// pages/tax/tax.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    userInfo: {}
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    console.log('onLoad test');
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
